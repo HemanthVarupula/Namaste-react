9c57f6628257ede9a24c1c64e49b28e367096220
Hemanth Varupula <varupulahemanth@gmailo.com>
2025-02-09 07:52:26 +0530

    ep1 ans ep2 are added

---
+++ episode1/App.js
@@ -0,0 +1,14 @@
+// const element=React.createElement("h1",{},"hii from react");
+//  const root=ReactDOM.createRoot(document.getElementById("root"));
+//  root.render(element);
+
+const element=[React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},[React.createElement("h1",{},"my name is harry"),React.createElement("h1",{},"my fav sport is cricket")],
+    React.createElement("div",{id:"child2"},[React.createElement("h1",{},"my name is harry"),React.createElement("h1",{},"my fav sport is cricket")],
+        
+        
+    )
+))];
+ const root=ReactDOM.createRoot(document.getElementById("root"));
+ root.render(element);
+ 
